a15bb3dac09693dd292379005c06cc4ecd85fbcf
zarakabbas642-web <zarakabbas642@gmail.com>
2026-05-09 16:12:37 +0500

    connect to database

--- Api/index.js
+++ Api/index.js
@@ -1,4 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MOGO).then(() => {
+  console.log('Connected to MongoDB!!');
+}).catch((err) => {
+  console.log('Error connecting to MongoDB!!', err);
+});
 
 const app = express();
 
